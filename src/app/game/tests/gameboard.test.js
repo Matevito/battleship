0f11423518c6595d2a4a70fb78d5555b2eb8c43a
Mateo <madiazt@unal.edu.co>
2021-06-20 22:19:07 -0500

    added todos test of gameboard

--- src/app/game/tests/gameboard.test.js
+++ src/app/game/tests/gameboard.test.js
@@ -1,1 +1,13 @@
 test.todo("put here gameboard tests")
+
+test.todo("plavce ships at specific and valid coordinates")
+
+test.todo("receive attack fails")
+
+test.todo("receive attack succed")
+
+test.todo("board tracjs missed attacks")
+
+test.todo("boards can report if all ships sunk")
+
+test.todo("board can report if not all ships sunk")
--- src/app/game/tests/ship.test.js
+++ src/app/game/tests/ship.test.js
@@ -2,6 +2,8 @@ import Ship from "../ship"
 
 test.todo("check that ship creates a valid object")
 
-test.todo("a ship receives a hit")
+test.todo("a ship can receive a hit")
 
 test.todo("a ship can sunk")
+
+test.todo("ship is created in a valid position")
