2dbe2c2b77bde9a4f7af8d41dd4b477465846277
Mateo <madiazt@unal.edu.co>
2021-06-21 19:04:00 -0500

    added skeleton logic of ship factory

--- src/app/game/ship.js
+++ src/app/game/ship.js
@@ -1,7 +1,29 @@
 
-const Ship = () => {
+const Ship = (length_size) => {
+    let length = length_size;
+    let life = length_size;
+    let sunk = false;
 
-    
+    const hit = () => {
+        life -= 1
+        if (life === 0){
+            sunk = true
+        }
+    }
+
+    const isSunk = () => {
+        return sunk
+    }
+
+    const shipLength = () => {
+        return length
+    }
+
+    const status = () => {
+        return life
+    }
+
+    return {hit, isSunk, shipLength, status}
 }
 
 export default Ship
--- src/app/game/tests/ship.test.js
+++ src/app/game/tests/ship.test.js
@@ -1,9 +1,27 @@
 import Ship from "../ship"
 
-test.todo("check that ship creates a valid object")
+let testShip_1 = new Ship(2)
 
-test.todo("a ship can receive a hit")
+test("a ship return it's valid length", () => {
+    expect(testShip_1.shipLength()).toBe(2)
+})
 
-test.todo("a ship can sunk")
+test("a ship can receive a hit", () => {
+    testShip_1.hit()
+    expect(testShip_1.status()).toBe(1)
+})
+
+test("a ship not sunk yet return false", () => {
+    expect(testShip_1.isSunk()).toBe(false)
+})
+
+test("a ship can sunk", () => {
+    //this kills a ship of 2 of length
+    testShip_1.hit()
+    expect(testShip_1.isSunk()).toBe(true)
+})
+
+//
+test.todo("a ship that is sunk cannot receive another hit")
 
 test.todo("ship is created in a valid position")
